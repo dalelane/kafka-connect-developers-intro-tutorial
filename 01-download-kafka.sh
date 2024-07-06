b3c1e2010e506dc09ace1feb8f44f11ef5e1a4a0
Dale Lane <dale.lane@uk.ibm.com>
2024-07-06 13:49:55 +0100

    chore: update version of Kafka

--- 01-download-kafka.sh
+++ 01-download-kafka.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-curl -o kafka.tgz -L https://downloads.apache.org/kafka/3.6.1/kafka_2.13-3.6.1.tgz
+curl -o kafka.tgz -L https://downloads.apache.org/kafka/3.7.1/kafka_2.13-3.7.1.tgz
 tar -xf kafka.tgz
 mv kafka_2.13-* kafka_home
 rm kafka.tgz
