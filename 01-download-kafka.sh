6ba919dc036b42d16b2ae689dedb6dc516111133
Dale Lane <dale.lane@uk.ibm.com>
2026-05-01 17:08:56 +0100

    chore: update version of Kafka
    Signed-off-by: Dale Lane <dale.lane@uk.ibm.com>

--- 01-download-kafka.sh
+++ 01-download-kafka.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-curl -o kafka.tgz -L https://downloads.apache.org/kafka/3.7.1/kafka_2.13-3.7.1.tgz
+curl -o kafka.tgz -L https://downloads.apache.org/kafka/4.2.0/kafka_2.13-4.2.0.tgz
 tar -xf kafka.tgz
 mv kafka_2.13-* kafka_home
 rm kafka.tgz
--- 02-run-kafka.sh
+++ 02-run-kafka.sh
@@ -5,24 +5,11 @@ if [ -z "$KAFKA_HOME" ]; then
 fi
 echo "KAFKA_HOME is set to $KAFKA_HOME"
 
-# Function to kill both scripts
-kill_scripts() {
-    echo "Terminating zookeeper and kafka..."
-    pkill -P $$  # Kill all child processes
-    exit 0
-}
+# Generate a Cluster UUID
+KAFKA_CLUSTER_ID="$($KAFKA_HOME/bin/kafka-storage.sh random-uuid)"
 
-# Trap the termination signal
-trap "kill_scripts" SIGINT SIGTERM
+# Format Log Directories
+$KAFKA_HOME/bin/kafka-storage.sh format --standalone -t $KAFKA_CLUSTER_ID -c $KAFKA_HOME/config/server.properties
 
-# Run zookeeper in the background
-$KAFKA_HOME/bin/zookeeper-server-start.sh $KAFKA_HOME/config/zookeeper.properties &
-
-# give it a moment to start up
-sleep 30
-
-# Run kafka in the background
-$KAFKA_HOME/bin/kafka-server-start.sh $KAFKA_HOME/config/server.properties &
-
-# block until ctrl-c
-wait
+# Start the Kafka Server
+$KAFKA_HOME/bin/kafka-server-start.sh $KAFKA_HOME/config/server.properties
